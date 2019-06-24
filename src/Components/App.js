15efddd1cafa9dc2f8f0349e7fe46cd03c068df2
miguelpc94 <miguelpdecastro@gmail.com>
2019-06-24 09:15:40 -0300

    Bootstrapping

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,9 +1,27 @@
 import React from 'react';
 import MemoryGame from './MemoryGame';
 
+const BSTest = () => {
+  return (
+    <div class='container'>
+      <div class='row'>
+        <div class='col-6'>
+          <div class='center-block'><p> This text is a test 1</p></div>
+        </div>
+        <div class='col-6'>
+        <div class='center-block'><p> This text is a test 2</p></div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
 function App() {
   return (
-    <MemoryGame />
+    <>
+      <MemoryGame />
+      <BSTest />
+    </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './bootstrap.min.css';
 import './index.css';
 import App from './Components/App';
 import * as serviceWorker from './serviceWorker';
